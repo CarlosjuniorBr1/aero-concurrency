d820ed2cd9f4765afa4f4288477ccdc4559c1d8c
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2026-07-25 15:18:46 -0300

    feat: classe para representar tabela, conexao e transação

--- aeroporto/src/main/java/org/air/database/DatabaseConnection.java
+++ aeroporto/src/main/java/org/air/database/DatabaseConnection.java
@@ -1,4 +1,15 @@
 package org.air.database;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DatabaseConnection {
+
+    private final int id;
+
+    private final ReentrantLock lock;
+
+    public DatabaseConnection(int id, ReentrantLock lock) {
+        this.id = id;
+        this.lock = lock;
+    }
 }
